f13752a614dc40d945195bf31e1da6473e0adaae
Filips Masolovs <filips.masolovs@gmail.com>
2021-05-15 13:49:09 +0300

    Initial code commit

---
+++ script/mainru.js
@@ -0,0 +1,6 @@
+(function() {
+    const copyright = document.getElementById('copyright');
+    const currentYear = new Date().getFullYear()
+    
+    copyright.innerHTML = `Copyright ${currentYear}, Grivika - Kravas pārvadājumi`
+}());
